7ac102e03587ff30c6d65681f9ab435374c1eb9a
Jaishir Bayuelo Crespo <jbayuelo@santaclaratech.es>
2020-03-28 10:14:07 -0500

    initial commit

--- app/backend/urls.py
+++ app/backend/urls.py
@@ -6,7 +6,7 @@ from rest_framework_swagger.views import get_swagger_view
 
 router = DefaultRouter()
 
-schema_view = get_swagger_view(title='Boilerplate Api')
+schema_view = get_swagger_view(title='Covid-backend Api')
 
 PREFIX_URL = settings.PREFIX_URL
 urlpatterns = [
